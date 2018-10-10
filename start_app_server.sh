17eaa85bcf90679f8909ac559dd2b1c55e288356
Peisi <pst6@students.waikato.ac.nz>
2018-10-10 19:49:20 +0800

    added

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
-
+# export MONGODB_CREDENTIALS=mongodb://{{USER}}:testtest1@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://kaskrex:testtest1@ds227853.mlab.com:27853/test1
 # finally, run the app in dev mode...
 npm run dev
