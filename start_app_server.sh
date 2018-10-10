f753bc28ab39d66a7c668806ec2411157f58fbf0
kaskrex <41402443+kaskrex@users.noreply.github.com>
2018-10-10 20:28:30 +0800

    Removed

--- start_app_server.sh
+++ start_app_server.sh
@@ -3,6 +3,6 @@
 
 # READ SECRET!
 # export MONGODB_CREDENTIALS=mongodb://{{USER}}:testtest1@ds666666.mlab.com:39999/some-database
-export MONGODB_CREDENTIALS=mongodb://kaskrex:testtest1@ds227853.mlab.com:27853/test1
+# export MONGODB_CREDENTIALS=mongodb://kaskrex:testtest1@ds227853.mlab.com:27853/test1
 # finally, run the app in dev mode...
 npm run dev
